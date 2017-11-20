eba6918c7ab74e96f3e02ee47fa60c8aa3e33b46
Aryeh Leib Taurog <git@aryehleib.com>
2017-11-20 15:38:09 +0200

    further attempt

--- getbranch.sh
+++ getbranch.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+git ls-remote origin
+git ls-remote origin | grep $TRAVIS_BRANCH
+git ls-remote origin | sed -n "s|$TRAVIS_COMMIT refs/heads/||p"
+git ls-remote origin | sed -n "s|$TRAVIS_COMMIT refs/heads/||p" | head -n1
+git ls-remote origin | sed -n "\|$TRAVIS_COMMIT refs/heads/|{s///p;q}"
 export REAL_BRANCH=$(git ls-remote origin | sed -n "\|$TRAVIS_COMMIT refs/heads/|{s///p;q}" )
 echo "'$REAL_BRANCH'"
 echo "'$TRAVIS_BRANCH'"
